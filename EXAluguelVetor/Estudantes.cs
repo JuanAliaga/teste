985f0988903db3bd18a22277fbc23a87d3902fa4
Juan Aliaga <juan.felipe.aliaga@gmail.com>
2020-02-04 09:36:02 -0300

    Exercicio de vetor com situação de aluguel

---
+++ EXAluguelVetor/Estudantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EXAluguelVetor
+{
+    class Estudantes
+    {
+        private string Name { get; set; }
+        private string Email { get; set; }
+
+        public Estudantes (string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + "," + Email;
+        }
+    }
+}
---
+++ EXAluguelVetor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EXAluguelVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] vect = new Estudantes[10];
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i =  1; i <= n; i++)
+            {
+                Console.WriteLine("Nome:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto:");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudantes(name, email);
+
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ":" + vect[i]);
+                }
+            }
+        }
+    }
+}
